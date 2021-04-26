ad4d191d9afbea2261dc3e16ef8f67e7b21ae6d6
SugarBlank <eneblank19@protonmail.com>
2021-04-25 21:33:34 -0400

    changed menu to array for less memory consumption

--- Plastico/Plastico.cs
+++ Plastico/Plastico.cs
@@ -1,78 +1,64 @@
 using System;
-using System.IO;
-using CommandLine;
+using System.Collections.Generic;
 
 namespace Plastico
 {
-    class Program
-    {
-        public interface ICommand
-        {
-            void Execute();
-            
-        }
+    class PlasticoTodoApp
+    {   
+        private string[] Menu = {
+        "        Plastico",
+        "------------------------",
+        "[1] Add Items",
+        "[2] Remove Items",
+        "[3] Print List",
+        "[4] Exit",
+        };
         
-        [Verb("remove", HelpText = "Remove item from the TodoList.")]
-        class RemoveOptions : ICommand{
-            [Option('i', "item", Required = true, HelpText = "Item to remove from Plastico.")]
-            public int Object { get; set; }
-            public void Execute()
-            {
-                Database DB = new Database();
-                DB.RemoveItem(Object);
-            }
-        }
-
-        [Verb("add", HelpText = "Add new item to Plastico.")]
-        class AddOptions : ICommand{
-            [Option('i', "item", Required = true, HelpText = "Item to add to list.")]
-            public string Item { get; set; }
-            [Option('d', "date", Required = false, HelpText = "Item to add to list.")]
-            public DateTime Date { get; set; }
-            
-            public void Execute()
-            {
-                if (Date == default(DateTime))
-                {
-                    Date = DateTime.Today;
-                }
-                Database DB = new Database();
-                DB.AddToDataBase(Item, Date);
-            }
-        }
-
-        [Verb("menu", isDefault: true, HelpText = "Shows the Main Menu for Plastico.")]
-        class Menu : ICommand{
-            public void Execute()
-            {
-                PlasticoTodoApp TodoApp = new PlasticoTodoApp();
-                do
-                TodoApp.MenuAction();
-                while (true);
-            }
-        }
-
-        [Verb("print", HelpText = "Prints all the items contained in the Plastico Database.")]
-        class Print : ICommand{
-            public void Execute()
-            {
-                Database.ReadDataBase();
-            }
-        }
-            
-        static void Main(string[] args)
+        public void MenuAction()
         {
-            if (!File.Exists(@"TodoList.db"))
+            for (int i = 0; i < Menu.Length; i++)
             {
-                Database DB = new Database();
-                DB.CreateDataBase();
-                Console.WriteLine("Made file at " + "TodoList.db");
+                Console.WriteLine(Menu[i]);
             }
-            else
+            int EnumChoice;
+            EnumChoice = Int32.Parse(Console.ReadLine());
+            MainMenu((Operations)EnumChoice);
+        }
+        private void MainMenu(Operations Choice)
+        { 
+            Database DB = new Database();
+            switch(Choice)
             {
-                TimeChecker.Check();
-                Database DB = new Database();
-                Parser.Default.ParseArguments<Menu, RemoveOptions, AddOptions, Print>(args).WithParsed<ICommand>(t => t.Execute());
+                case Operations.AddItems:
+                    Console.WriteLine("\nAdd a new item to the list:");
+                    string NewItem = Console.ReadLine();
+                    Console.WriteLine("\nEnter the date.");
+                    DateTime Date = DateTime.Parse(Console.ReadLine());
+                    DB.AddToDataBase(NewItem, Date);
+                    Console.ReadKey();
+                    Console.Clear();
+                    GC.Collect();
+                    break;
+
+                case Operations.RemoveItems:
+                    Console.WriteLine("\nInsert the number of the item you wish to remove:");
+                    int ItemToRemove = Int32.Parse(Console.ReadLine());
+                    DB.RemoveItem(ItemToRemove);
+                    Console.ReadKey();
+                    Console.Clear();
+                    GC.Collect();
+                    break;
+
+                case Operations.PrintItems:
+                    Database.ReadDataBase();
+                    Console.ReadKey();
+                    Console.Clear();
+                    GC.Collect();
+                    break;
+
+                case Operations.Exit:
+                    Environment.Exit(0);
+                    break;
             }
         }
     }
